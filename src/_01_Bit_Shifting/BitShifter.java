af11b0d70a57c85b93d19918b95c31589a1fd4cc
codeavenger1 <shivakansagara@gmail.com>
2020-07-26 17:29:05 -0700

    finished 00, 01, 02; working on 03
    figure out what to put in the parameters in the main method next class

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -1,17 +1,26 @@
 package _01_Bit_Shifting;
 
+import _00_Binary_Conversion.DecimalToBinary;
+
 public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
+		
 		int num = 4;
 		int numShifted = num << 1;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
+		System.out.println(numShifted);
+	
+		System.out.println(DecimalToBinary.convertDecimalToBinary(numShifted));
 		
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
+		//Shifts the number one binary place. 4 became 8.
 		
 		// 4. Try shifting num 3 places.
-		
+		int x = 16;
+		int xShifted = x >>> 1;
+		System.out.println(xShifted);
 		// FYI: Binary values can be shifted to the right as well using the >> operator.	
 	}
 }
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -1,25 +1,42 @@
 package _03_Printing_Binary;
 
 public class BinaryPrinter {
-	//Complete the methods below so they print the passed in parameter in binary.
-	//Use bit shifting and bit masking to print the binary numbers.
-	//Do not use the Integer.toBinaryString method!
-	//Don't be afraid to use the methods that are already complete to finish the others.
-	//Create a main method to test your methods.
-	
+	// Complete the methods below so they print the passed in parameter in binary.
+	// Use bit shifting and bit masking to print the binary numbers.
+	// Do not use the Integer.toBinaryString method!
+	// Don't be afraid to use the methods that are already complete to finish the
+	// others.
+	// Create a main method to test your methods.
+
+	public static void main(String[] args) {
+		BinaryPrinter print = new BinaryPrinter();
+		//print.printByteBinary(4);
+	}
+
 	public void printByteBinary(byte b) {
-	
+		//8
+		String binary = "";
+		
+		while(b != 0) {
+		if (b % 2 != 0) {
+			binary += '1';
+		} else {
+			binary += '0';
+		}
+		
+		System.out.println(binary);
+		}
 	}
-	
+
 	public void printShortBinary(short s) {
-		
+		// 16
 	}
-	
+
 	public void printIntBinary(int i) {
-		
+		// 32
 	}
-	
+
 	public void printLongBinary(long l) {
-		
+		// 64
 	}
 }
